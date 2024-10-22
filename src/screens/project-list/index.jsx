0e006749aa74c63d8fd6952201442e484b6ca3b7
c <2655774991@qq.com>
2024-10-22 20:41:48 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,38 @@
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useState, useEffect } from "react";
+import { cleanObject } from "utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (res) => {
+        if (res.ok) {
+          setList(await res.json());
+        }
+      },
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json());
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+export const isFalsy = (val) => (val === 0 ? false : !val);
+
+export const cleanObject = (obj) => {
+  const result = { ...obj };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
